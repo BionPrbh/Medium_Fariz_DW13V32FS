c1bd0b466ef303930d889ee1fbf4e5d275ee6b7a
BionPrbh <mufhabu@gmail.com>
2019-12-09 14:34:09 +0700

    deleted unused code in textinput.js

--- src/components/input/text/textinput.js
+++ src/components/input/text/textinput.js
@@ -14,23 +14,3 @@ class TextInput extends Component {
 }
 
 export default TextInput;
-
-// export default function InputWithIcon() {
-//   const classes = useStyles();
-
-//   return (
-//     <div>
-//       <FormControl className={classes.margin}>
-//         <InputLabel htmlFor="input-with-icon-adornment">With a start adornment</InputLabel>
-//         <Input
-//           id="input-with-icon-adornment"
-//           startAdornment={
-//             <InputAdornment position="middle">
-//               <AccountCircle />
-//             </InputAdornment>
-//           }
-//         />
-//       </FormControl>
-//     </div>
-//   );
-// }
