6c4aab63f7d5298b2d384a4c36276bee0a2c4cd1
BionPrbh <mufhabu@gmail.com>
2019-12-12 00:13:38 +0700

    fix login and register

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react'
-import './login.css'
+import React, { Component } from "react";
+import "./login.css";
 import Container from "@material-ui/core/Container";
 import InputWithIcon from "../input/text/textinput";
-import LoginButton from "../input/button/button"
+import LoginButton from "../input/button/button";
+import {Link} from 'react-router-dom';
+
 
 class Login extends Component {
-    render() {
-        return(
-            <div className="Login">
-              <Container className="ContentContainerLeft" />
-              <Container className="ContentContainerMiddle">
-                <h1>Sign in with email</h1>
-                <p>Enter the email address associated with your account, and we'll send a magic link to your inbox.</p>
-                <div>
-                  <form className="LoginForm">
-                    <InputWithIcon 
-                      textInputClassName="TextInputEmail"
-                      textInputLabel="Your email"
-                    />
-                    <InputWithIcon 
-                      textInputClassName="TextInputPassword"
-                      textInputLabel="Your password"
-                    />
-                    <LoginButton 
-                      ButtonClassName="SigninButton"
-                      ButtonLabel="Continue"
-                    />
-                    <LoginButton 
-                      ButtonClassName="AllSignUpOption"
-                      ButtonLabel="All sign up options"
-                    />
-                  </form>  
-                </div>
-                <svg width="25" height="25"></svg>
-              </Container>
-              <Container className="ContentContainerRight" />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Login" style={{ marginTop: 100 }}>
+        <Container className="ContentContainerLeft" />
+        <Container className="ContentContainerMiddle">
+          <h1>Sign in with email</h1>
+          <p>
+            Enter the email address associated with your account, and we'll send
+            a magic link to your inbox.
+          </p>
+          <div>
+            <form className="LoginForm">
+              <InputWithIcon
+                textInputClassName="TextInputEmail"
+                textInputLabel="Your email"
+              />
+              <InputWithIcon
+                textInputClassName="TextInputPassword"
+                textInputLabel="Your password"
+              />
+              <Link to="/home">
+                <LoginButton
+                  ButtonClassName="SigninButton"
+                  ButtonLabel="Continue"
+                />
+              </Link>
+              <Link to="/register">
+                <LoginButton
+                  ButtonClassName="AllSignUpOption"
+                  ButtonLabel="All sign up options"
+                />
+              </Link>         
+              </form>
+          </div>
+          <svg width="25" height="25"></svg>
+        </Container>
+        <Container className="ContentContainerRight" style={{paddingRight:0, paddingLeft: 0}}/>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
--- src/components/register/register.js
+++ src/components/register/register.js
@@ -3,49 +3,52 @@ import "./register.css";
 import Container from "@material-ui/core/Container";
 import InputWithIcon from "../input/text/textinput";
 import RegisterButton from "../input/button/button"
+import {Link} from 'react-router-dom';
 
 class Register extends Component {
   render() {
     return (
-      <div className="Register">
+      <div className="Register" style={{marginTop:100}}>
         <Container className="ContentContainerLeft" />
-        <Container className="ContentContainerMiddle">
-          <h1>Join Medium.</h1>
-          <p className="MiddleContentParagraph1">
-            Create an account to receive great stories in your inbox,
-            personalize your homepage and follow authors and topics that you
-            love.
-          </p>
-          <div>
-            <form className="RegisterForm">
-              <InputWithIcon
-                textInputClassName="TextInputUsername"
-                textInputLabel="Your username"
+          <Container className="ContentContainerMiddle">
+            <h1>Join Medium.</h1>
+            <p className="MiddleContentParagraph1">
+              Create an account to receive great stories in your inbox,
+              personalize your homepage and follow authors and topics that you
+              love.
+            </p>
+            <div>
+              <form className="RegisterForm">
+                <InputWithIcon
+                  textInputClassName="TextInputUsername"
+                  textInputLabel="Your username"
+                />
+                <InputWithIcon
+                  textInputClassName="TextInputPassword"
+                  textInputLabel="Your password"
+                />
+                <InputWithIcon
+                  textInputClassName="TextInputEmail"
+                  textInputLabel="Your email"
+                />
+              </form>
+            </div>
+            <Link to="/home">
+              <RegisterButton 
+                ButtonClassName="RegisterButton"
+                ButtonLabel="Continue"
               />
-              <InputWithIcon
-                textInputClassName="TextInputPassword"
-                textInputLabel="Your password"
-              />
-              <InputWithIcon
-                textInputClassName="TextInputEmail"
-                textInputLabel="Your email"
-              />
-            </form>
-          </div>
-          <RegisterButton 
-            ButtonClassName="RegisterButton"
-            ButtonLabel="Registration"
-          />
-          <p className="MiddleContentParagraph3">
-            Already have an account?<a href="/login">Sign in</a>
-          </p>
-          <p className="MiddleContentParagraph3">
-            To make medium work, we log user data and share it with service
-            providers. Click "Sign Up" above to accept Medium's <a className="TermsOfServiceLink" href="/">Terms of Service</a>
-            & <a className="PrivacyPolicyLink" href="/">Privacy Policy</a>.
-          </p>
-        </Container>
-        <Container className="ContentContainerRight" />
+            </Link>
+            <p className="MiddleContentParagraph3">
+              Already have an account?<a href="/login">Sign in</a>
+            </p>
+            <p className="MiddleContentParagraph3">
+              To make medium work, we log user data and share it with service
+              providers. Click "Sign Up" above to accept Medium's <a className="TermsOfServiceLink" href="/">Terms of Service</a>
+              & <a className="PrivacyPolicyLink" href="/">Privacy Policy</a>.
+            </p>
+          </Container>
+        <Container className="ContentContainerRight" style={{paddingRight:0, paddingLeft: 0}}/>
       </div>
     );
   }
