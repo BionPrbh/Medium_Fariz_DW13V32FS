24ec2c2abac3a84b2d281ec9a671df06627af3a4
BionPrbh <mufhabu@gmail.com>
2019-12-17 01:05:32 +0700

    added bookmarks

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 // import Comment from './components/comment/comment'
 // import NewStory from './screen/newStory/newStory'
 // import Stats from './screen/stats/stats'
-import Story from './screen/story/story'
+// import Story from './screen/story/story'
 // import Profile from './components/profile/profile'
 // import ArticleByPerson from './screen/articleByPerson/articleByPerson'
-// import Bookmarks from './screen/bookmarks/bookmarks'
+import Bookmarks from './components/bookmarks/bookmarks'
 // import Follow from './components/follow/follow' 
 // import ArticleDetails from './screen/article/articleDetails'
 // import RelatedArticle from './components/relatedArticle/relatedArticle'
@@ -32,7 +32,7 @@ function App() {
             <CategoryPage />
           </Route> */}
           <Route path="/">
-            <Story />
+            <Bookmarks />
             {/* <Home /> */}
           </Route>
           {/* <Route path="/articleDtails"> 
