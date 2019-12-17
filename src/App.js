09447096fe208eb955332c1462c9821943a7ad8f
BionPrbh <mufhabu@gmail.com>
2019-12-17 11:59:19 +0700

    fixed app routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-// import Login from "./screen/login/login"
-// import Register from "./screen/register/register";
-// import CategoryPage from './screen/categoryPage/categoryPage'
-// import Home from './screen/home/home'
-// import ArticleDetails from './screen/articleDetails/articleDetails'
-// import Comment from './components/comment/comment'
-// import NewStory from './screen/newStory/newStory'
-// import Stats from './screen/stats/stats'
-// import Story from './screen/story/story'
-// import Profile from './components/profile/profile'
+import Login from "./screen/login/login"
+import Register from "./screen/register/register";
+import CategoryPage from './screen/categoryPage/categoryPage'
+import Home from './screen/home/home'
+import ArticleDetails from './screen/articleDetails/articleDetails'
+import Comment from './components/comment/comment'
+import NewStory from './screen/newStory/newStory'
+import Stats from './screen/stats/stats'
+import Story from './screen/story/story'
+import Profile from './components/profile/profile'
 import ArticleByPerson from './screen/articleByPerson/articleByPerson'
-// import Bookmarks from './components/bookmarks/bookmarks'
-// import Follow from './components/follow/follow' 
-// import ArticleDetails from './screen/article/articleDetails'
-// import RelatedArticle from './components/relatedArticle/relatedArticle'
+import Bookmarks from './components/bookmarks/bookmarks'
+import Follow from './components/follow/follow' 
+import ArticleDetails from './screen/article/articleDetails'
+import RelatedArticle from './components/relatedArticle/relatedArticle'
 
 function App() {
   return (
     <div>
       <Router>
         <Switch >
-          {/* <Route path="/login">
+          <Route path="/login">
             <Login />
           </Route>
           <Route path="/register">
@@ -30,32 +30,40 @@ function App() {
           </Route>
           <Route path="/categoryPage">
             <CategoryPage />
-          </Route> */}
-          <Route path="/">
-            <ArticleByPerson />
-            {/* <Home /> */}
           </Route>
-          {/* <Route path="/articleDtails"> 
+          <Route path="/articleDtails"> 
             <ArticleDetails />
-          </Route> */}
-          {/* <Route path="/follow">
+          </Route>
+          <Route path="/follow">
             <Follow />
-          </Route> */}
-          {/* <Router path="/comment">
+          </Route>
+          <Router path="/comment">
             <Comment />
-          </Router> */}
-          {/* <Router path="/menuRouter">
+          </Router>
+          <Router path="/menuRouter">
             <RelatedArticle />
-          </Router> */}
-          {/* <Router path="/profile">
+          </Router>
+          <Router path="/profile">
             <Profile />
-          </Router> */}
-          {/* <Router path="/newStory">
+          </Router>
+          <Router path="/newStory">
             <NewStory />
-          </Router> */}
-          {/* <Route path="/bookmarks">
+          </Router>
+          <Route path="/bookmarks">
             <Bookmarks />
-          </Route> */}
+          </Route>
+          <Route path="/articleByPerson">
+            <ArticleByPerson />
+          </Route>
+          <Route path="/story">
+            <Story />
+          </Route>
+          <Route path="/stats">
+            <Stats />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
       </Router>
     </div>
